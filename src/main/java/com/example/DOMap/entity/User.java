a2f34aafefd27cd87280fbb32addb6496cbd4676
문정훈 <mjh090801abc@gmail.com>
2026-04-09 19:32:22 +0900

    사용자 비밀번호 컬럼을 만드는 코드 작성(값이 비어있을 수 없음)

--- src/main/java/com/example/DOMap/entity/User.java
+++ src/main/java/com/example/DOMap/entity/User.java
@@ -14,8 +14,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // 사용자 아이디 값이 비어있으며, 고유값이어야함
+    // 사용자 아이디 값이 비어있을 수 없으며, 고유값이어야함
     @Column(nullable = false, unique = true)
     private String username;
+
+    // 비밀번호 저장(값이 비어있을 수 없음)
+    @Column(nullable = false)
+    private String password;
 
 }
