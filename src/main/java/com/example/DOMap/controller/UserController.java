90322ba5c8a0ec79f5bdf8f651f0467a89e7349c
문정훈 <mjh090801abc@gmail.com>
2026-04-09 23:41:42 +0900

    추가된 각 어노테이션들의 기능을 주석으로 설명

--- src/main/java/com/example/DOMap/controller/UserController.java
+++ src/main/java/com/example/DOMap/controller/UserController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // 데이터를 JSON 형태로 반환할때 활용
-@RequiredArgsConstructor //
-@RequestMapping("/api")
+@RequiredArgsConstructor // final이 붙은 필드를 모아서 자동으로 생성사 생성(DI)
+@RequestMapping("/api") // 모은 주소는 "/api"로 시작
 public class UserController {
 
 }
