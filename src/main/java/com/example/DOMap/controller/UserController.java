ce134959182f8dab66331ec2524094b9411a34cc
문정훈 <mjh090801abc@gmail.com>
2026-04-09 23:40:38 +0900

    이 서버는 데이터를 주고받는 API서버임을 명시하고, 모든 주소의 시작을 "/API"로 시작하게 만드는 코드들을 추가

---
+++ src/main/java/com/example/DOMap/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.example.DOMap.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // 데이터를 JSON 형태로 반환할때 활용
+@RequiredArgsConstructor //
+@RequestMapping("/api")
+public class UserController {
+
+}
