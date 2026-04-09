d3ab455b174140761831f99c87c641af9aca6024
문정훈 <mjh090801abc@gmail.com>
2026-04-09 21:24:18 +0900

    Optional<User>를 한 뒤 useranem으로 조회가 가능하도록 코드 추가

--- src/main/java/com/example/DOMap/repository/UserRepository.java
+++ src/main/java/com/example/DOMap/repository/UserRepository.java
@@ -3,6 +3,13 @@ package com.example.DOMap.repository;
 import com.example.DOMap.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 // <관리하려는 Entity, Entity의 @Id의 PK 타입>
-public interface UserRepository extends JpaRepository<User,Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // Optional<User> : User 데이터가 있을 수도 없을 수도 있다는 뜻(만약에 값이 없으면 예외를 던676767666ㅋ)
+    // 쿼리 메서드 SELECT * FROM user WHERE username = ? 라는 SQL이 자동으로 생성
+    Optional<User> findByUsername(String username);
+
 }
