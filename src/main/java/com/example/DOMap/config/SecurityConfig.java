6543a4831a585108c11bc6fcdb729ded4f8a144d
문정훈 <mjh090801abc@gmail.com>
2026-04-09 23:20:10 +0900

    Security에서 사용되는 어노테이션과 passwordEncoder메서드에 들어간 함수에 대한 주석 추가

--- src/main/java/com/example/DOMap/config/SecurityConfig.java
+++ src/main/java/com/example/DOMap/config/SecurityConfig.java
@@ -5,11 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
+@Configuration // 스프링의 설정 정보 파일인 것을 알림
 public class SecurityConfig {
 
-    @Bean
+    @Bean // BCryptPasswordEncoder객체를 스프링 컨테이너가 관리하는 객체로 등록
     public PasswordEncoder passwordEncoder() {
+        // 비밀번호를 안전하게 암호화하는 함수를 사용
         return new BCryptPasswordEncoder();
     }
 }
