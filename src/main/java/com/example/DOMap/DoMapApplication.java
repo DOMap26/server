d00bb60e04c96377b38edf780c90e8b45e91b3c8
문정훈 <mjh090801abc@gmail.com>
2026-04-09 14:19:50 +0900

    test commit

---
+++ src/main/java/com/example/DOMap/DoMapApplication.java
@@ -0,0 +1,13 @@
+package com.example.DOMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DOMap/DoMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DOMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
