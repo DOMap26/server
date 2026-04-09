900c68c0a1f6494efe289ef6f4f8b71048ce5426
문정훈 <mjh090801abc@gmail.com>
2026-04-09 21:36:30 +0900

    비밀번호와 비밀번호 확인을 위한 비밀번호를 비교하여서 예외를 던지는 코드 추가

--- src/main/java/com/example/DOMap/UserService.java
+++ src/main/java/com/example/DOMap/UserService.java
@@ -1,5 +1,6 @@
 package com.example.DOMap;
 
+import com.example.DOMap.dto.SignupRequestDto;
 import com.example.DOMap.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,5 +13,12 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-
+    // 비밀번호 확인
+    public void signup(SignupRequestDto request) {
+        // 사용자가 입력한 비밀번호가 비밀번호 확인위한 비밀번호와 같지 않다면
+        if (!request.getPassword().equals(request.getConfirmPassword())) {
+            // 예외를 던짐
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+    }
 }
