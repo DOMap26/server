fa3379eab17c5f8249e83eb16afdf522a2460851
문정훈 <mjh090801abc@gmail.com>
2026-04-10 23:52:18 +0900

    토큰 주인, 토큰 발급 시간, 만료 시간 설정을 하는 코드 추가

--- src/main/java/com/example/DOMap/jwt/JwtUtil.java
+++ src/main/java/com/example/DOMap/jwt/JwtUtil.java
@@ -1,10 +1,24 @@
 package com.example.DOMap.jwt;
 
-// JWT 생성/검증 담당 클래스
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
 public class JwtUtil {
 
     private static final String SECRET_KEY = "mysecretkey";
 
-    private static final long EXPIRE_TIME = 1000 * 60 * 60;
+    private static final long EXPIRATION_TIME = 1000 * 60 * 60; // 1시간
 
+    public static String createToken(String username) {
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
 }
