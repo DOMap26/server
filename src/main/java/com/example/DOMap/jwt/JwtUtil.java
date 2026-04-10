496446bad1781ba6f10e0463fc1cc2adf1af33ee
문정훈 <mjh090801abc@gmail.com>
2026-04-10 23:13:25 +0900

    토큰 암호화 키 지정 + 토큰 유효시간 지정

---
+++ src/main/java/com/example/DOMap/jwt/JwtUtil.java
@@ -0,0 +1,9 @@
+package com.example.DOMap.jwt;
+
+// JWT 생성/검증 담당 클래스
+public class JwtUtil {
+
+    private static final String SECRET_KEY = "mysecretkey";
+
+    private static final long EXPIRE_TIME = 1000 * 60 * 60;
+}
