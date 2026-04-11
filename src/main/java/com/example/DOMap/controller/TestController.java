05dd65155152319805f621698e4be40b14d9e122
문정훈 <mjh090801abc@gmail.com>
2026-04-12 02:07:42 +0900

    @RestController를 붙이지 않아서 controller 인식 실패였던 코드에 @RestController 추가

--- src/main/java/com/example/DOMap/controller/TestController.java
+++ src/main/java/com/example/DOMap/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.example.DOMap.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class TestController {
     @GetMapping("/api/test")
     public String test() {
