2ee58f184988aa011254fa99cb8ad6d02909aba4
문정훈 <mjh090801abc@gmail.com>
2026-04-12 20:09:41 +0900

    Entity에 필요한 생성자 및 다른 어노테이션들 추가

---
+++ src/main/java/com/example/DOMap/entity/Player.java
@@ -0,0 +1,14 @@
+package com.example.DOMap.entity;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Player {
+
+}
