cfe0336989278c938388ffa9e92167fd198d7ea1
문정훈 <mjh090801abc@gmail.com>
2026-04-12 20:19:05 +0900

    User 1명 = Player 1명으로 정의하기 위한 @OneToOne 어노테이션 사용 후 name은 임시로 정하고 빈값일 수 없으 유일값으로 지정하고 User 클래스를 user로 지정하는 코드 추가

--- src/main/java/com/example/DOMap/entity/Player.java
+++ src/main/java/com/example/DOMap/entity/Player.java
@@ -1,19 +1,26 @@
 package com.example.DOMap.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.*;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Entity
+@Entity // 이 클래스가 DB 테이블이 된다는 뜻
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Player {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB가 id 자동 증가
     private Long id;
+
+    @OneToOne // User 하나당 Player 하나
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
 }
